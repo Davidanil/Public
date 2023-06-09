45746cfd71ccd5a5a8085c57e7fc8f63117f550e
Davidanil <5599050+Davidanil@users.noreply.github.com>
2023-06-09 20:00:16 +0100

    Add files via upload

---
+++ scrapers/FlipperZero.ps1
@@ -0,0 +1,37 @@
+﻿#SIMPLE SCRIPT TO SCRAPE THE FLIPPERZERO STORE
+
+$url = "https://shop.flipperzero.one"
+$url2 = "https://lab401.com/products/flipper-zero?variant=42927883485414"
+$alarm = "directory for the alarm"
+$interval = 30
+
+while ($true) {
+    # Retrieve the webpage HTML
+    $html = (Invoke-WebRequest -Uri $url).Content
+    $html2 = (Invoke-WebRequest -Uri $url2).Content
+
+    # Check if the HTML contains the words "sold out"
+    if ($html -notmatch "sold out") {
+        # Display a notification if the words "sold out" are not found
+        [Windows.Forms.MessageBox]::Show("FlipperZero.one - BUY FLIPPER ZERO NOW!!!", "Notification")
+        (New-Object Media.SoundPlayer $alarm).PlaySync()
+        Write-Host "FlipperZero Shop - IN STOCK"
+    }
+    else {
+        Write-Host "FlipperZero Shop - SOLD OUT"
+    }
+
+        # Check if the HTML contains the words "sold out"
+    if ($html2 -notmatch "Sold Out") {
+        # Display a notification if the words "sold out" are not found
+        [Windows.Forms.MessageBox]::Show("LAB401 - BUY FLIPPER ZERO NOW!!!", "Notification")
+        (New-Object Media.SoundPlayer $alarm).PlaySync()
+        Write-Host "LAB401 - IN STOCK"
+
+    }
+    else {
+        Write-Host "LAB401 - SOLD OUT"
+    }
+    # Wait for the specified interval before checking again
+    Start-Sleep -Seconds $interval
+}
